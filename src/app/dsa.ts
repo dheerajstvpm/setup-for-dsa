8529d2e589911cee25bfd2c4aeb5bbc93e324291
officialmailofdheeraj <officialmailofdheeraj@gmail.com>
2024-07-20 18:48:23 +0000

    update question

--- src/app/dsa.ts
+++ src/app/dsa.ts
@@ -9,9 +9,32 @@ export interface IDsa {
 export const dsa: IDsa = {
   linkedList: {
     id: '1',
-    question: `qweqweqwe
+    question: `/**
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+ */
       `,
-    solution: `123`,
+    solution: `
+    const arr = [10, 15, 3, 7];
+    <br/>const k = 17;
+    <br/>function babu(arr,k){
+    <br/>let result = false;
+    <br/>const complement = new Set();
+    <br/>arr.forEach(item => {
+    <br/>    if (complement.has(k - item)) {
+    <br/>       result = true;
+    <br/>       return;
+    <br/>    }
+    <br/>    complement.add(item)
+    <br/>})
+    <br/>return result;
+    <br/>}
+    <br/>babu(arr,k)`,
   },
   doublyLinkedList: {
     id: '2',
